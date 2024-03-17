d5f2f64c32491ba78fc8528d67fd40eecdef0ef8
muza97 <mustafa.zaitoun97@gmail.com>
2024-03-17 02:58:37 +0100

    LoginScreen Alost done will work on the Login page next

---
+++ screens/WelcomeScreen.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import {View, Text, Image, TouchableOpacity} from  "react-native";
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { themeColors } from '../theme';
+import { useNavigation } from '@react-navigation/native';
+
+export default function WelcomeScreen() {
+    const navigation = useNavigation()
+  return (    
+<SafeAreaView  className="flex-1" style={{backgroundColor: themeColors.bgColor(1)}}>
+        <View className="flex-1 flex justify-around my-4">
+            <Text
+            className="text-gray font-bold text-4xl text-center">
+                Let's Get Started
+            </Text>
+            <View className="flex-row justify-center">
+            <Image source={require('../assets/image/mmm.webp')} 
+                style={{width: 350, height: 350, borderRadius: 20}} />
+            </View> 
+             <View className="space-y-4">
+                <TouchableOpacity
+                onPress={()=> navigation.navigate('SignUp')}
+                className="py-3 bg-yellow-400 mx-7 rounded-xl">
+                    <Text
+                        className="text-xl font-bold text-center text-gray-700"
+                    >
+                         SignUp 
+                    </Text>
+                </TouchableOpacity>
+                <View className="flex-row justify-center">
+                <Text className="text-white font-semibold">Alredy have an account?</Text>
+                    <TouchableOpacity onPress={()=> navigation.navigate('Login')}>
+                        <Text className="font-semibold text-yellow-400">Log in</Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
+        </View>
+    </SafeAreaView>
+  );
+}
---
+++ theme/index.js
@@ -0,0 +1,5 @@
+export const themeColors = {
+    text: '#f97316',
+    bgColor: (opacity) => `rgba(130, 129, 241, ${opacity})` 
+  };
+  
