1ce84bbe776b46444e4a94c6d4714e5776b83ae8
muza97 <mustafa.zaitoun97@gmail.com>
2024-04-11 01:51:31 +0200

    My trips

--- screens/TripsScreen.js
+++ screens/TripsScreen.js
@@ -1,10 +1,46 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import React from 'react';
+import { View, Text, TouchableOpacity, FlatList } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+import { useNavigation } from '@react-navigation/native';
 
 export default function TripsScreen() {
+  const navigation = useNavigation();
+  const tripsData = [
+    // ex åkturerdata
+    { id: '1', address: '348 Fillmore St, San Francisco, CA', time: '6:45 PM, Monday June 19', driver: 'Ying', price: '$20' },
+    { id: '1', address: '348 Fillmore St, San Francisco, CA', time: '6:45 PM, Monday June 19', driver: 'Ying', price: '$20' },
+    { id: '1', address: '348 Fillmore St, San Francisco, CA', time: '6:45 PM, Monday June 19', driver: 'Ying', price: '$20' },
+    { id: '1', address: '348 Fillmore St, San Francisco, CA', time: '6:45 PM, Monday June 19', driver: 'Ying', price: '$20' },
+    { id: '1', address: '348 Fillmore St, San Francisco, CA', time: '6:45 PM, Monday June 19', driver: 'Ying', price: '$20' },
+    { id: '1', address: '348 Fillmore St, San Francisco, CA', time: '6:45 PM, Monday June 19', driver: 'Ying', price: '$20' },
+
+    // ... fler åkturer ...
+  ];
+
   return (
-    <View>
-      <Text>TripsScreen</Text>
+    <View className="flex-1 bg-white pt-10">
+      <View className="flex-row items-center p-4 border-b border-gray-300">
+        <TouchableOpacity onPress={() => navigation.goBack()} className="mr-2">
+          <Ionicons name="arrow-back" size={30} className="text-black" />
+        </TouchableOpacity>
+        <Text className="text-xl font-bold flex-1">My Trips</Text>
+      </View>
+
+      <FlatList
+        data={tripsData}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          <TouchableOpacity className="flex-row items-center p-4 border-b border-gray-300">
+            <Ionicons name="location-outline" size={24} className="text-gray-600" />
+            <View className="flex-1 ml-2">
+              <Text className="text-lg font-semibold">{item.address}</Text>
+              <Text className="text-gray-500">{item.time} with {item.driver}</Text>
+            </View>
+            <Text className="text-gray-900 font-semibold">{item.price}</Text>
+            <Ionicons name="chevron-forward" size={24} className="text-gray-600" />
+          </TouchableOpacity>
+        )}
+      />
     </View>
-  )
+  );
 }
