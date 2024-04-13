ae32e80e282979c2a17943f866ac3db26cd696db
muza97 <mustafa.zaitoun97@gmail.com>
2024-04-13 21:47:27 +0200

    polyline

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,14 +1,17 @@
 //screens/HomeScreen.js
 import React, { useState, useEffect, useRef, useCallback } from 'react';
 import { View, TouchableOpacity } from 'react-native';
-import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
+import MapView, { PROVIDER_GOOGLE, Marker, Polyline } from 'react-native-maps';
 import { FontAwesome, MaterialIcons } from '@expo/vector-icons';
 import { useNavigation } from '@react-navigation/native';
 import { themeColors } from '../theme';
 import * as Location from 'expo-location';
 import BottomSheetComponent from '../components/BottomSheetComponent';
 import RideSummaryBox from '../components/RideSummaryBox'; // Import the component
-
+import useGeocoding from '../hooks/useGeocoding';
+import { GEOCODING_API_KEY } from '@env'; // Ensure this is set up in your .env file
+import polyline from '@mapbox/polyline'; // Make sure to install the @mapbox/polyline package
+ 
 export default function HomeScreen() {
   const navigation = useNavigation();
   const [userLocation, setUserLocation] = useState(null);
@@ -19,8 +22,26 @@ export default function HomeScreen() {
   const [showSummaryBox, setShowSummaryBox] = useState(false);
   const [pickupAddress, setPickupAddress] = useState(''); // Add state for pickup address
   const [dropoffAddress, setDropoffAddress] = useState(''); // Add state for dropoff address
+  const { geocodeAddress, startCoordinates, destinationCoordinates, error } = useGeocoding();
+  const [route, setRoute] = useState(null);
 
-
+  const getDirections = async (startLoc, destinationLoc) => {
+    try {
+      const response = await fetch(
+        `https://maps.googleapis.com/maps/api/directions/json?origin=${startLoc.latitude},${startLoc.longitude}&destination=${destinationLoc.latitude},${destinationLoc.longitude}&key=${GEOCODING_API_KEY}`
+      );
+      const json = await response.json();
+      const points = polyline.decode(json.routes[0].overview_polyline.points);
+      const coordinates = points.map(point => ({
+        latitude: point[0],
+        longitude: point[1],
+      }));
+      setRoute(coordinates);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  
   const handleFocus = useCallback(() => {
     setIsBottomSheetOpen(!isBottomSheetOpen);
     bottomSheetRef.current?.snapToIndex(isBottomSheetOpen ? 0 : 1); // Toggle between 10% and 70%
@@ -45,6 +66,17 @@ export default function HomeScreen() {
     })();
   }, []);
 
+  const onAddressChange = (field, value) => {
+    if (field === 'pickUp') {
+      setPickupAddress(value);
+      geocodeAddress(value, 'start');
+    } else if (field === 'dropOff') {
+      setDropoffAddress(value);
+      geocodeAddress(value, 'destination');
+    }
+  };
+
+
   const getCurrentLocation = async () => {
     let location = await Location.getCurrentPositionAsync({});
     const { latitude, longitude } = location.coords;
@@ -55,6 +87,12 @@ export default function HomeScreen() {
       longitudeDelta: 0.005,
     });
   };
+
+  useEffect(() => {
+    if (startCoordinates && destinationCoordinates) {
+      getDirections(startCoordinates, destinationCoordinates);
+    }
+  }, [startCoordinates, destinationCoordinates]);
 
   return (
     <View className="flex-1 justify-center items-center bg-[color:themeColors.bgColor(1)]">
@@ -68,7 +106,43 @@ export default function HomeScreen() {
           longitudeDelta: 0.0421,
         }}
         region={userLocation}
-      />
+      >
+        {/* Show user location marker */}
+        {userLocation && (
+          <Marker
+            coordinate={userLocation}
+            title="Your Location"
+          />
+        )}
+  
+        {/* Show pickup location marker */}
+        {startCoordinates && (
+          <Marker
+            coordinate={startCoordinates}
+            title="Pick Up"
+            pinColor="green"
+          />
+        )}
+  
+        {/* Show dropoff location marker */}
+        {destinationCoordinates && (
+          <Marker
+            coordinate={destinationCoordinates}
+            title="Drop Off"
+            pinColor="blue"
+          />
+        )}
+  
+        {/* Polyline between pickup and dropoff */}
+        {startCoordinates && destinationCoordinates && (
+           <Polyline
+      coordinates={route}
+      strokeColor="#000" // black or any color you prefer
+      strokeWidth={6}
+    />
+  )}
+      </MapView>
+      
       <TouchableOpacity
         onPress={() => navigation.toggleDrawer()}
         className="absolute top-10 left-4 z-10"
@@ -91,157 +165,22 @@ export default function HomeScreen() {
         <MaterialIcons name="travel-explore" size={30} color="black" />
       </TouchableOpacity>
       <BottomSheetComponent
-    ref={bottomSheetRef}
-    // Make sure to pass a function that updates the state for pickup and dropoff addresses
-    onAddressChange={(field, value) => {
-      if (field === 'pickUp') {
-        setPickupAddress(value);
-      } else if (field === 'dropOff') {
-        setDropoffAddress(value);
-      }
-    }}
-    onRequestRide={handleRequestRide}
-  />
-
-  {showSummaryBox && (
-    <RideSummaryBox
-      pickupAddress={pickupAddress}
-      dropoffAddress={dropoffAddress}
-      distance="2.5 miles"
-      rate="5"
-      onRequestConfirm={() => {
-        console.log('Ride confirmed');
-        setShowSummaryBox(false);
-      }}
-    />
-  )}
-
+        ref={bottomSheetRef}
+        onAddressChange={onAddressChange}
+        onRequestRide={handleRequestRide}
+      />
+  
+      {showSummaryBox && (
+        <RideSummaryBox
+          pickupAddress={pickupAddress}
+          dropoffAddress={dropoffAddress}
+          distance="2.5 miles"
+          rate="5"
+          onRequestConfirm={() => {
+            console.log('Ride confirmed');
+            setShowSummaryBox(false);
+          }}
+        />
+      )}
     </View>
-  );
-}
-// import React, { useState, useEffect, useRef, useCallback } from 'react';
-// import { View, TouchableOpacity, Platform, PermissionsAndroid, Text, Button} from 'react-native';
-// import MapView, { PROVIDER_GOOGLE, Marker, Polyline } from 'react-native-maps';
-// import { FontAwesome } from '@expo/vector-icons'; 
-// import { useNavigation } from '@react-navigation/native';
-// import useGeocoding from '../hooks/useGeocoding';
-// import { themeColors } from '../theme';
-// import * as Location from 'expo-location';
-// import BottomSheetComponent from '../components/BottomSheetComponent'; 
-// import { MaterialIcons } from '@expo/vector-icons';
-// import RideSummaryBox from '../components/RideSummaryBox';
-
-
-
-// export default function HomeScreen() {
-//   const navigation = useNavigation(); 
-//   const { geocodeAddress } = useGeocoding();
-//   const [userLocation, setUserLocation] = useState(null);
-//   const [isLocationArrowPressed, setIsLocationArrowPressed] = useState(false);
-//   const [routeCoordinates, setRouteCoordinates] = useState([]);
-//   const [startCoordinates, setStartCoordinates] = useState(null);
-//   const [destinationCoordinates, setDestinationCoordinates] = useState(null);
-//   const bottomSheetRef = useRef(null);
-//   const [isBottomSheetOpen, setIsBottomSheetOpen] = useState(false);
-//   const [rideRequested, setRideRequested] = useState(false);
-
-//   const handleFocus = useCallback(() => {
-//     const targetIndex = isBottomSheetOpen ? 0 : 1;  // Use isBottomSheetOpen to toggle
-//     bottomSheetRef.current?.snapToIndex(targetIndex);
-//     setIsBottomSheetOpen(!isBottomSheetOpen);  // Toggle the state
-//   }, [isBottomSheetOpen]);
-
-
-// const handleRequestRide = useCallback(() => {
-//   // This would be triggered when the Request Ride button is pressed
-//   setRideRequested(true);
-// }, []);
-
-//   // const toggleBottomSheet = () => {
-//   //   if (isBottomSheetOpen) {
-//   //     bottomSheetRef.current?.close(0);  // Close to minimum snap point instead of completely closing
-//   //   } else {
-//   //     bottomSheetRef.current?.expand(2);  // Expand to the next snap point or fully open
-//   //   }
-//   //   setIsBottomSheetOpen(!isBottomSheetOpen);
-//   // };
-
-//   useEffect(() => {
-//     (async () => {
-//       let { status } = await Location.requestForegroundPermissionsAsync();
-//       if (status !== 'granted') {
-//         console.log('Permission to access location was denied');
-//         return;
-//       }
-
-//       getCurrentLocation();
-//     })();
-//   }, []);
-
-// const getCurrentLocation = async () => {
-//     let location = await Location.getCurrentPositionAsync({});
-//     const { latitude, longitude } = location.coords;
-//     setUserLocation({
-//       latitude,
-//       longitude,
-//       latitudeDelta: 0.005,
-//       longitudeDelta: 0.005,
-//     });
-//   };
-
-
-//   return (
-//     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: themeColors.bgColor(1) }}>
-//       <MapView
-//         provider={PROVIDER_GOOGLE}
-//         style={{ width: '100%', height: '100%' }}
-//         initialRegion={userLocation || {
-//           latitude: 37.78825, 
-//           longitude: -122.4324, 
-//           latitudeDelta: 0.0922,
-//           longitudeDelta: 0.0421,
-//         }}
-//         region={userLocation} // re-center the map to userLocation 
-//       >
-//         {/* Marker and Polyline as before */}
-//       </MapView>
-//       <TouchableOpacity 
-//         onPress={() => navigation.toggleDrawer()} 
-//         className="absolute top-10 left-4" // Tailwind CSS classes for positioning
-//       >
-//         <FontAwesome name="bars" size={30} color="black" />
-//       </TouchableOpacity>
-//       <TouchableOpacity
-//         onPress={() => {
-//           setIsLocationArrowPressed(!isLocationArrowPressed);
-//           getCurrentLocation(); 
-//         }}
-//         className="absolute top-10 right-4" 
-//       >
-//         <FontAwesome name="location-arrow" size={30} color={isLocationArrowPressed ? "red" : "black"} />
-//       </TouchableOpacity>
-//       <TouchableOpacity 
-//         onPress={handleFocus} 
-//         className="absolute top-12 self-center"
-//       >
-//         <MaterialIcons name="travel-explore" size={30} color="black" />
-//       </TouchableOpacity>
-//       <BottomSheetComponent
-//         ref={bottomSheetRef}
-//         onFocus={handleFocus}
-//       />
-//       {/* <BottomSheetComponent
-//         ref={bottomSheetRef}
-//         onFocus={handleFocus}
-//         onRequestRide={handleRequestRide}
-//       />
-//       <TouchableOpacity 
-//         onPress={() => handleFocus('pickUp')}  
-//         className="absolute top-12 self-center"
-//       >
-//         <MaterialIcons name="travel-explore" size={30} color="black" />
-//       </TouchableOpacity>
-//       <BottomSheetComponent ref={bottomSheetRef} />
-//       {isBottomSheetOpen ? <Text>Bottom Sheet is Open</Text> : <Text>Bottom Sheet is Closed to 10%</Text>} */}
-//     </View>
-//   );}
+  );}
